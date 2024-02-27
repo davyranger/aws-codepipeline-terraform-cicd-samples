e7ff2ddc1544cfaa1fe175d55843689738f664c8
Strain <david.strain@accenture.com>
2024-02-27 13:40:55 +0800

    commented out code

--- main.tf
+++ main.tf
@@ -4,17 +4,37 @@
 #http://aws.amazon.com/agreement or other written agreement between Customer and either
 #Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
+#configure the aws provider
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.20.1"
+      version = "5.38.0"
     }
   }
-
 }
+
+################################
+#initialise the backend
+################################
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-health"
+    key            = "statefolder/terraform.tfstate"
+    region         = "us-west-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 
 #Module for creating a new S3 bucket for storing pipeline artifacts
 module "s3_artifacts_bucket" {
@@ -30,97 +50,97 @@ module "s3_artifacts_bucket" {
   }
 }
 
-# Resources
+# # Resources
 
-# Module for Infrastructure Source code repository
-module "codecommit_infrastructure_source_repo" {
-  source = "./modules/codecommit"
+# # Module for Infrastructure Source code repository
+# module "codecommit_infrastructure_source_repo" {
+#   source = "./modules/codecommit"
 
-  create_new_repo          = var.create_new_repo
-  source_repository_name   = var.source_repo_name
-  source_repository_branch = var.source_repo_branch
-  repo_approvers_arn       = var.repo_approvers_arn
-  kms_key_arn              = module.codepipeline_kms.arn
-  tags = {
-    Project_Name = var.project_name
-    Environment  = var.environment
-    Account_ID   = local.account_id
-    Region       = local.region
-  }
+#   create_new_repo          = var.create_new_repo
+#   source_repository_name   = var.source_repo_name
+#   source_repository_branch = var.source_repo_branch
+#   repo_approvers_arn       = var.repo_approvers_arn
+#   kms_key_arn              = module.codepipeline_kms.arn
+#   tags = {
+#     Project_Name = var.project_name
+#     Environment  = var.environment
+#     Account_ID   = local.account_id
+#     Region       = local.region
+#   }
 
-}
+# }
 
-# Module for Infrastructure Validation - CodeBuild
-module "codebuild_terraform" {
-  depends_on = [
-    module.codecommit_infrastructure_source_repo
-  ]
-  source = "./modules/codebuild"
+# # Module for Infrastructure Validation - CodeBuild
+# module "codebuild_terraform" {
+#   depends_on = [
+#     module.codecommit_infrastructure_source_repo
+#   ]
+#   source = "./modules/codebuild"
 
-  project_name                        = var.project_name
-  role_arn                            = module.codepipeline_iam_role.role_arn
-  s3_bucket_name                      = module.s3_artifacts_bucket.bucket
-  build_projects                      = var.build_projects
-  build_project_source                = var.build_project_source
-  builder_compute_type                = var.builder_compute_type
-  builder_image                       = var.builder_image
-  builder_image_pull_credentials_type = var.builder_image_pull_credentials_type
-  builder_type                        = var.builder_type
-  kms_key_arn                         = module.codepipeline_kms.arn
-  tags = {
-    Project_Name = var.project_name
-    Environment  = var.environment
-    Account_ID   = local.account_id
-    Region       = local.region
-  }
-}
+#   project_name                        = var.project_name
+#   role_arn                            = module.codepipeline_iam_role.role_arn
+#   s3_bucket_name                      = module.s3_artifacts_bucket.bucket
+#   build_projects                      = var.build_projects
+#   build_project_source                = var.build_project_source
+#   builder_compute_type                = var.builder_compute_type
+#   builder_image                       = var.builder_image
+#   builder_image_pull_credentials_type = var.builder_image_pull_credentials_type
+#   builder_type                        = var.builder_type
+#   kms_key_arn                         = module.codepipeline_kms.arn
+#   tags = {
+#     Project_Name = var.project_name
+#     Environment  = var.environment
+#     Account_ID   = local.account_id
+#     Region       = local.region
+#   }
+# }
 
-module "codepipeline_kms" {
-  source                = "./modules/kms"
-  codepipeline_role_arn = module.codepipeline_iam_role.role_arn
-  tags = {
-    Project_Name = var.project_name
-    Environment  = var.environment
-    Account_ID   = local.account_id
-    Region       = local.region
-  }
+# module "codepipeline_kms" {
+#   source                = "./modules/kms"
+#   codepipeline_role_arn = module.codepipeline_iam_role.role_arn
+#   tags = {
+#     Project_Name = var.project_name
+#     Environment  = var.environment
+#     Account_ID   = local.account_id
+#     Region       = local.region
+#   }
 
-}
+# }
 
-module "codepipeline_iam_role" {
-  source                     = "./modules/iam-role"
-  project_name               = var.project_name
-  create_new_role            = var.create_new_role
-  codepipeline_iam_role_name = var.create_new_role == true ? "${var.project_name}-codepipeline-role" : var.codepipeline_iam_role_name
-  source_repository_name     = var.source_repo_name
-  kms_key_arn                = module.codepipeline_kms.arn
-  s3_bucket_arn              = module.s3_artifacts_bucket.arn
-  tags = {
-    Project_Name = var.project_name
-    Environment  = var.environment
-    Account_ID   = local.account_id
-    Region       = local.region
-  }
-}
-# Module for Infrastructure Validate, Plan, Apply and Destroy - CodePipeline
-module "codepipeline_terraform" {
-  depends_on = [
-    module.codebuild_terraform,
-    module.s3_artifacts_bucket
-  ]
-  source = "./modules/codepipeline"
+# module "codepipeline_iam_role" {
+#   source                     = "./modules/iam-role"
+#   project_name               = var.project_name
+#   create_new_role            = var.create_new_role
+#   codepipeline_iam_role_name = var.create_new_role == true ? "${var.project_name}-codepipeline-role" : var.codepipeline_iam_role_name
+#   source_repository_name     = var.source_repo_name
+#   kms_key_arn                = module.codepipeline_kms.arn
+#   s3_bucket_arn              = module.s3_artifacts_bucket.arn
+#   tags = {
+#     Project_Name = var.project_name
+#     Environment  = var.environment
+#     Account_ID   = local.account_id
+#     Region       = local.region
+#   }
+# }
+# # Module for Infrastructure Validate, Plan, Apply and Destroy - CodePipeline
+# module "codepipeline_terraform" {
+#   depends_on = [
+#     module.codebuild_terraform,
+#     module.s3_artifacts_bucket
+#   ]
+#   source = "./modules/codepipeline"
 
-  project_name          = var.project_name
-  source_repo_name      = var.source_repo_name
-  source_repo_branch    = var.source_repo_branch
-  s3_bucket_name        = module.s3_artifacts_bucket.bucket
-  codepipeline_role_arn = module.codepipeline_iam_role.role_arn
-  stages                = var.stage_input
-  kms_key_arn           = module.codepipeline_kms.arn
-  tags = {
-    Project_Name = var.project_name
-    Environment  = var.environment
-    Account_ID   = local.account_id
-    Region       = local.region
-  }
-}
+#   project_name          = var.project_name
+#   source_repo_name      = var.source_repo_name
+#   source_repo_branch    = var.source_repo_branch
+#   s3_bucket_name        = module.s3_artifacts_bucket.bucket
+#   codepipeline_role_arn = module.codepipeline_iam_role.role_arn
+#   stages                = var.stage_input
+#   kms_key_arn           = module.codepipeline_kms.arn
+#   tags = {
+#     Project_Name = var.project_name
+#     Environment  = var.environment
+#     Account_ID   = local.account_id
+#     Region       = local.region
+#   }
+# }
